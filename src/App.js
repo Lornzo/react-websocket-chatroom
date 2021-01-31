821f6bc2bd815c13fe3dc708a1d9437476a6f352
Lornzo <a6288678@hotmail.com>
2021-01-31 22:12:00 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react'
+import ReactDom from 'react-dom'
+// import webSocket from 'socket.io-client'
 
-function App() {
+
+const App = () => {
+  const [ws,setWs] = useState(null)
+  const [msg,setMsg] = useState("")
+
+  // 連線到WebSocket
+  const connectWebSocket = () => {
+    if(!ws){
+      setWs(new WebSocket("ws://127.0.0.1:9527/ws"))
+    }else{
+      console.log("已經連線，所以不動作")
+    }
+  }
+
+  // 初始化WebSocket的動作
+  const initWebSocket = () => {
+    if(ws){
+
+      // 連線成功
+      ws.onopen = ()=>{
+        console.log("連線成功",ws)
+        ws.send("小夫，我要進來囉!")
+      }
+
+      //連線關閉
+      ws.onclose = ()=>{
+        console.log("關閉連線",ws)
+      }
+
+      // 在接到Server發進來的訊息
+      ws.onmessage = (event) => {
+        console.log("來自Server:",event)
+      }
+
+    }
+  }
+
+  //向Server傳東西
+  const sendMessage = () => {
+    if(ws && msg){
+      ws.send(msg)
+    }
+  }
+  
+
+  useEffect(()=>{
+    if(ws){
+
+        console.log("WebSocket連線中...")
+
+        // 初始化websocket對應的動作
+        initWebSocket()
+
+    }
+  },[ws])
+
+  const inputMessage = (event)=>{
+    setMsg(event.target.value)
+  }
+
+  // 最終Render
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <input type="text" onChange={inputMessage}></input>
+        <input type='button' value='連線' onClick={connectWebSocket} />
+        <input type='button' value='送出訊息' onClick={sendMessage} />
     </div>
-  );
+  )
 }
-
 export default App;
